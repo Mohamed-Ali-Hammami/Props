832d11f60f4bf51bf73aec1c9a31b2a865a58a54
mohamed-ali-hammami <dali.654@hotmail.fr>
2022-03-17 11:51:54 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Data from "./Profile/Component";
+import Image from "./Gilg.jpg"
 
 function App() {
+  
+  let fun = {fullname:"Mohamed Ali Hammami",bio:"Hello world",profession:"webdevelopper"}
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Data data={fun}>
+        <img src={Image}/>
+        </Data>
+      
     </div>
   );
 }
